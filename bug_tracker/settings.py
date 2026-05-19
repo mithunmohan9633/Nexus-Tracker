14ca88cc9e9bb18320e0108702b1195802e08508
Mithun Mohan <mithunmohan9633@users.noreply.github.com>
2026-05-20 00:04:58 +0530

    Use whitenoise to serve static files

--- bug_tracker/settings.py
+++ bug_tracker/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -140,3 +141,5 @@ LOGIN_URL = 'login'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
